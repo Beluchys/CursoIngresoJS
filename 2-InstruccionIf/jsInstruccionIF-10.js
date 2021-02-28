94df75d57cdd0f5bc36533a842493fdfaed3c74a
unknown <gmarquez@iplan.com.ar>
2021-02-28 19:08:01 -0300

    Se modifico correctamente

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,28 @@
+/*Al presionar el Botón, asignar una nota RANDOM al examen y mostrar:
+"EXCELENTE" para notas igual a 9 o 10,
+"APROBÓ" para notas mayores a 4,
+"Vamos, la proxima se puede" para notas menores a 4*/
+
+
+
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	let notaRandom;
+	let maximo = 10;
+	let minimo = 1;
 
-}//FIN DE LA FUNCIÓN
+	notaRandom = Math.round(Math.random () * (maximo - minimo) + minimo);
+
+	if (notaRandom >=9) 
+	{
+		alert("Su nota es "+notaRandom+". Excelente");
+	}
+	else if (notaRandom >= 4 && notaRandom <= 8) 
+	{
+		alert("Su nota es "+notaRandom+". Aprobó");
+	} 
+	else
+	{
+		alert("Su nota es "+notaRandom+". Vamos, la proxima se puede!")
+	}
+}
