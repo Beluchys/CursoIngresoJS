68c3847a41b00976d93e6f1673235c91db1e6505
unknown <gmarquez@iplan.com.ar>
2021-02-28 16:32:55 -0300

    Se modifico Correctamente

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,16 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	var importeInicial;
+	var descuento25 = 0.75;
+	var importeConDescuento;
+
+	importeInicial = document.getElementById("txtIdImporte").value;
+
+	importeInicial = parseInt(importeInicial);
+
+	importeConDescuento = importeInicial * descuento25;
+
+	document.getElementById("txtIdResultado").value = importeConDescuento; 
+
 }
