c7c304b72aba6e4d3b0be9334b8439bb2d8ca7ac
unknown <gmarquez@iplan.com.ar>
2021-02-28 19:31:38 -0300

    Se modifico correctamente

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,27 @@
+/*al seleccionar un mes informar.
+si es Enero: "que comiences bien el año!!!."
+si es Marzo: "a clases!!!."
+si es Julio: "se vienen las vacaciones!!!."
+si es Diciembre: "Felices fiesta!!!."*/
+
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	var mesDelAño;
 
+	mesDelAño = document.getElementById("txtIdMes").value;
 
-
-}//FIN DE LA FUNCIÓN
+	switch (mesDelAño) {
+		case "Enero":
+			alert("Que comiences bien el año!");
+			break;
+		case "Marzo":
+			alert("A clases!");
+			break;
+		case "Julio":
+			alert("Se vienen las vacaciones!");
+			break;
+		case "Diciembre":
+			alert("Felices fiestas!");
+			break;
+	}
+}
