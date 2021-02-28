b1c3065f6ed3045c8b003f8b13c78a9a075989f8
unknown <gmarquez@iplan.com.ar>
2021-02-28 17:26:09 -0300

    Se modifico correctamente

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,21 @@
+/*Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", 
+mostrar el siguiente mensaje: 'Es muy pequeño para NO ser soltero.'
+*/
+
+
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	var edadIngresada;
+	var estadoCivil;
 
+	edadIngresada = document.getElementById("txtIdEdad").value;
+	edadIngresada = parseInt(edadIngresada);
 
-}//FIN DE LA FUNCIÓN
+	estadoCivil = document.getElementById("estadoCivil").value;
+
+	if (edadIngresada <=17 && estadoCivil != "Soltero") 
+	{
+		alert("Es muy pequeño para NO ser soltero.");
+	}
+}
--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,20 @@
+/*
+Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", NO HACER NADA,
+pero si no es asi, y es soltero y no es menor, mostrar el siguiente mensaje: 'Es soltero y no es menor.
+*/
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	var edadIngresada;
+	var estadoCivil;
 
+	edadIngresada = document.getElementById("txtIdEdad").value;
+	edadIngresada = parseInt(edadIngresada);
 
-}//FIN DE LA FUNCIÓN
+	estadoCivil = document.getElementById("estadoCivil").value;
+
+	if (edadIngresada >= 18 && estadoCivil == "Soltero")
+	{
+		alert("Es soltero y no es menor");	
+	}
+}
