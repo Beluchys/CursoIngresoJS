ec92f7a070b5a55613ed9d9f8681a2b28d47c428
unknown <gmarquez@iplan.com.ar>
2021-03-01 20:40:38 -0300

    Se modifico correctamente

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -3,24 +3,34 @@ Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
 function mostrar()
 {	// declarar variables
-	var banderaDelPrimero;
 	var numeroIngresado;
 	var numeroMaximo;
 	var numeroMinimo;
 	var respuesta = true;
-	//iniciar variables
-	banderaDelPrimero="es el primero";
+	var flag = 0;
 
 	do
 	{
 		numeroIngresado = prompt("Ingrese un Número");
 		numeroIngresado = parseInt(numeroIngresado);
+		if(flag == 0)
+		{
+			numeroMaximo = numeroIngresado;
+			numeroMinimo = numeroIngresado;
+			flag = 1;
+		}
+		if(numeroIngresado > numeroMaximo)
+		{
+			numeroMaximo=numeroIngresado;
+		}
+		else if (numeroIngresado<numeroMinimo)
+		{
+			numeroMinimo=numeroIngresado;
+		}
 
 		respuesta=confirm("¿Desea continuar?");
 	}while(respuesta);
 
-
-
 	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
+	txtIdMinimo.value=numeroMinimo;
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -12,18 +12,69 @@ hasta que el usuario quiera, mostrar:
 9-Diferencia entre positivos y negativos, (positvos-negativos). */
 function mostrar()
 {
-	//declarar contadores y variables 
-	var respuesta;
-	var numeroIngresado;
-	var sumaNegativos=0;
+	//declarar contadores y variables
+	var numeroIngresado; 
+	var respuesta = true;
+	var contadorNegativos = 0;
+	var contadorPositivos = 0;
+	var contadorCeros = 0;
+	var contadorPares = 0;
+	var promedioPositivos;
+	var promedioNegativos;
+	var acumuladorNegativos=0;
+	var acumuladorPositivos=0;
+	var diferencia;
 
-	respuesta="si";
 
-	while(respuesta=="si")
-	{
+	do
+	{ //primero --> Codigo dentro del bucle - pedír numeros
+		numeroIngresado = prompt("Ingrese un número");
+		numeroIngresado = parseInt(numeroIngresado);
+
+		// analizar el signo del número y actuar en consecuencia
+
+		if(numeroIngresado < 0) //Negativos
+		{
+			acumuladorNegativos = acumuladorNegativos + numeroIngresado;
+			contadorNegativos++;
+		}
+		else if(numeroIngresado == 0) //ceros
+		{
+			contadorCeros++;
+		}
+		else //positivos
+		{
+			acumuladorPositivos = acumuladorPositivos + numeroIngresado;
+			contadorPositivos++;
+		}
+			
+		if (numeroIngresado % 2 == 0) //saber si es par
+		{
+			contadorPares++;
+		}
+		respuesta=confirm("¿Desea Continuar?");
+	}while(respuesta);
+
+	// hacer los calculos con los datos obtenido dentro del bucle
+	promedioPositivos = acumuladorPositivos /contadorPositivos;
+	promedioNegativos = acumuladorNegativos /contadorNegativos;
+	diferencia = contadorPositivos - contadorNegativos;
+
+	document.write("la suma de los negativos es :"+acumuladorNegativos);
+
+	document.write("la suma de los positivos es :"+acumuladorPositivos);
 		
-		respuesta=prompt("desea continuar?");
-	}//fin del while
+	document.write("la cantidad de los positivos es :"+contadorPositivos);
 
-	document.write("la suma de negativos es :"+sumaNegativos);
+	document.write("la cantudad de los negativos es :"+contadorNegativos);
+
+	document.write("la cantidad de ceros es :"+contadorCeros);
+
+	document.write("la cantidad de numero pares es :"+contadorPares);
+
+	document.write("El promedio de los positivos es :"+promedioPositivos);
+	
+	document.write("El promedio de los negativos es :"+promedioNegativos);
+	
+	document.write("la diferencia entre positivo y negativo es :"+diferencia);
 }//FIN DE LA FUNCIÓN
