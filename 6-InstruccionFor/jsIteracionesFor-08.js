3f86509ca65a2ee068b8b1ef731effea9a1f6d7a
unknown <gmarquez@iplan.com.ar>
2021-03-02 12:03:07 -0300

    Se modifico correctamente

--- 6-InstruccionFor/jsIteracionesFor-08.js
+++ 6-InstruccionFor/jsIteracionesFor-08.js
@@ -1,7 +1,28 @@
+/*
+al presionar el botón pedir un número. Informar si el numero es PRIMO o no.
+*/
+
+
 function mostrar()
 {
+	let numeroIngresado;
+	let	contadorDivisores =0;
 
+	numeroIngresado = prompt("Ingrese un Número");
+	numeroIngresado = parseInt(numeroIngresado);
 
-	alert("ok");
-
+	for (let contador = 1; contador <= numeroIngresado; contador++) 
+	{
+		if (numeroIngresado % contador == 0) 
+		{
+			contadorDivisores++;
+		}
+	}
+	if (contadorDivisores == 2)
+	{
+		alert(numeroIngresado+" Es un número Primo");
+	}else
+	{
+		alert(numeroIngresado+" El número no es primo")
+	}
 }//FIN DE LA FUNCIÓN
